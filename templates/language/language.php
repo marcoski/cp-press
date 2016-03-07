f5c5f2ff9dfb3e743b2e5bb9cebfceccc43c965c
marcoski <mtrognon@gmail.com>
2016-03-07 16:01:25 +0100

    0.7.0 -- Added catalan flag

--- templates/language/language.php
+++ templates/language/language.php
@@ -3,10 +3,10 @@ use Commonhelp\Util\Inflector;
 wp_nonce_field('save', '_cppress_multilanguage_nonce');
 echo '<select name="cp-press-country">';
 foreach($flags as $img){
-	$code = strtoupper(basename($img, '.svg'));
-	if(isset($countries[$code])){
-		$country = Inflector::humanize(strtolower($countries[$code]), ' ');
-		echo '<option value="' . strtolower($code) . '"' . selected($selectedCountry, strtolower($code)) . '>' . $country . '</option>';
+	$code = basename($img, '.svg');
+	if(isset($languages[$code])){
+		$lang = $languages[$code];
+		echo '<option value="' . strtolower($code) . '"' . selected($selectedCountry, strtolower($code)) . '>' . $lang . '</option>';
 	}
 }
 echo '</select>';
